61055433ec9f989d0d45986928d68264eb997bd8
iamsaramj <saramekshaj19@gmail.com>
2024-03-26 18:44:40 +0100

    Initial commit

---
+++ SpringBootFirstEx/src/test/java/com/example/SpringBootFirstEx/SpringBootFirstExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootFirstEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
