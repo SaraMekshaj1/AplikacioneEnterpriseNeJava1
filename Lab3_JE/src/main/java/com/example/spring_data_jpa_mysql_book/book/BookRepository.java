6daf90e5fc9cb9ea209210dce78e776855883c89
SaraMekshaj1 <153858360+SaraMekshaj1@users.noreply.github.com>
2024-04-15 17:36:17 +0200

    Delete Lab3_JE directory

--- Lab3_JE/src/main/java/com/example/spring_data_jpa_mysql_book/book/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.spring_data_jpa_mysql_book.book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
-
