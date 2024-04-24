6e4c5d22310c88d421b868f62fefe03522a9e66d
SaraMekshaj1 <153858360+SaraMekshaj1@users.noreply.github.com>
2024-04-24 03:03:14 +0200

    Add files via upload

---
+++ Lab3_JE/src/main/java/com/example/spring_data_jpa_mysql_book/book/Book.java
@@ -0,0 +1,40 @@
+package com.example.spring_data_jpa_mysql_book.book;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ Lab3_JE/src/main/java/com/example/spring_data_jpa_mysql_book/book/BookService.java
@@ -0,0 +1,41 @@
+package com.example.spring_data_jpa_mysql_book.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book updateBook(Long id, Book updatedBook) {
+        if (bookRepository.existsById(id)) {
+            updatedBook.setId(id);
+            return bookRepository.save(updatedBook);
+        }
+        return null;
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
